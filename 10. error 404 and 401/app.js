d241ef80b50222a3cad606e3130236c435b98a71
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2022-09-08 19:02:46 +0530

    fetch api error

--- 10. error 404 and 401/app.js
+++ 10. error 404 and 401/app.js
@@ -1,23 +1,31 @@
-var inputTxt = document.querySelector(".input-txt");
-var getBtn = document.querySelector(".get-btn");
+var getBtn404 = document.querySelector(".get-btn-404");
+var getBtn401 = document.querySelector(".get-btn-401");
 var outputBox = document.querySelector(".output-box");
 
 
 // function errorHandler(error){
 //     outputBox.innerText = "error occured: " + error;
 // }
+var url_403 = "https://www.googleapis.com/youtube/v3/search";
+var url_404  ="https://jsonplaceholder.typicode.com/users/32";
 
-var url  ="https://jsonplaceholder.typicode.com/users/32";
-function getBtnEventHandler(){
-
- var textUrl = url + "?" + "text=" + inputTxt.value;
- fetch(textUrl)
- .then(response => {
-    if(response.status === 404){
-        console.log("page not found");
-     }
- })
- 
+function getBtn404EventHandler(){
+ fetch(url_404)
+  .then((res) => {
+    if (res.status === 404) {
+        outputBox.innerText = "page not found";
+    }
+  });
 }
 
-getBtn.addEventListener("click", getBtnEventHandler);
+function getBtn401EventHandler(){
+    fetch(url_403)
+    .then((res) => {
+      if (res.status === 403) {
+          outputBox.innerText = "this user is not allowed";
+      }
+    });
+}
+
+getBtn404.addEventListener("click", getBtn404EventHandler);
+getBtn401.addEventListener("click", getBtn401EventHandler);
