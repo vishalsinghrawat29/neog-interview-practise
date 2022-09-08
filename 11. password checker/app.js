27a737ed87a7ef348209310b44885cd1efa13ea2
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2022-09-08 20:07:59 +0530

    Update app.js

--- 11. password checker/app.js
+++ 11. password checker/app.js
@@ -2,16 +2,30 @@ var inputTxt = document.querySelector(".input-txt");
 var submitBtn = document.querySelector(".submit-btn");
 var outputBox = document.querySelector(".output-box");
 
+
 function submitBtnEventHandler(){
     var inputValue = inputTxt.value;
-    if((inputValue.split("").length) < 10){
+    if((inputValue.length) < 10){
+        submitBtn.disabled = true;
         outputBox.innerText = "error";
         inputTxt.style.backgroundColor = "red";
     }else{
+        submitBtn.disabled = false;
         outputBox.innerText = "success";
+        inputTxt.style.backgroundColor = "green";       
+    }
+}
+
+function submitBtnInputEventHandler(){
+    var inputValue = inputTxt.value;
+    if((inputValue.length) < 10){
         submitBtn.disabled = true;
-        inputTxt.style.backgroundColor = "green";
+        inputTxt.style.border = " 0.1rem solid red ";
+    }else{
+        submitBtn.disabled = false;
+        inputTxt.style.border = " 0.1rem solid green ";
     }
 }
 
 submitBtn.addEventListener("click", submitBtnEventHandler);
+inputTxt.addEventListener("input", submitBtnInputEventHandler);
